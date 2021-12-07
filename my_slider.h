f0d497e25be8036454875ae2946772d226adf27d
Krzysztof-Kamola <94909773+Krzysztof-Kamola@users.noreply.github.com>
2021-12-07 15:48:46 +0000

    Add files via upload
    needed to run

---
+++ my_slider.h
@@ -0,0 +1,26 @@
+#ifndef MY_SLIDER_H
+#define MY_SLIDER_H
+
+#include <QObject>
+#include <QWidget>
+#include <QSlider>
+
+class My_Slider : public QSlider
+{
+
+
+
+
+
+
+
+public:
+    My_Slider():QSlider(NULL){}
+
+
+
+
+public slots:
+};
+
+#endif // MY_SLIDER_H
